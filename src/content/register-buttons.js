d975360f8140dc7e84ce8a7e9c764319e61cffce
beninsydney <benodev@protonmail.com>
2022-04-10 18:57:42 +1000

    WIP

--- src/content/register-buttons.js
+++ src/content/register-buttons.js
@@ -3,13 +3,20 @@ module.exports = {
     if (req.urlPath !== '/account/register') {
       return
     }
-    const form = document.getElementById('submit-form')
-    const buttonContainer = document.createElement('div')
+    const link = pageDoc.createElement('link')
+    link.attr = {
+      rel: 'stylesheet',
+      href: '/public/oauth.css'
+    }
+    const head = pageDoc.getElementsByTagName('head')[0]
+    head.child.push(link)
+    const form = pageDoc.getElementById('submit-form')
+    const buttonContainer = pageDoc.createElement('div')
     buttonContainer.attr = {
       id: 'oauth-buttons',
       class: 'oauth-buttons'
     }
     form.child[form.child.length] = form.child[form.child.length - 1]
-    form.child[form.child.length - 1] = buttonContainer
+    form.child[form.child.length - 2] = buttonContainer
   }
 }
