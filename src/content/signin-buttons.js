56f40d8d1d61e131bbd29fe036c4567aff8022cf
beninsydney <benodev@protonmail.com>
2022-04-28 08:44:17 +1000

    Use named methods for debugging

--- src/content/signin-buttons.js
+++ src/content/signin-buttons.js
@@ -1,22 +1,24 @@
 module.exports = {
-  page: async (req, res, pageDoc) => {
-    if (req.urlPath !== '/account/signin') {
-      return
-    }
-    const link = pageDoc.createElement('link')
-    link.attr = {
-      rel: 'stylesheet',
-      href: '/public/oauth.css'
-    }
-    const head = pageDoc.getElementsByTagName('head')[0]
-    head.child.push(link)
-    const form = pageDoc.getElementById('submit-form')
-    const buttonContainer = pageDoc.createElement('div')
-    buttonContainer.attr = {
-      id: 'oauth-buttons',
-      class: 'oauth-buttons'
-    }
-    form.child[form.child.length] = form.child[form.child.length - 1]
-    form.child[form.child.length - 2] = buttonContainer
+  page: signinButtons
+}
+
+async function signinButtons (req, _, pageDoc) {
+  if (req.urlPath !== '/account/signin') {
+    return
   }
+  const link = pageDoc.createElement('link')
+  link.attr = {
+    rel: 'stylesheet',
+    href: '/public/oauth.css'
+  }
+  const head = pageDoc.getElementsByTagName('head')[0]
+  head.child.push(link)
+  const form = pageDoc.getElementById('submit-form')
+  const buttonContainer = pageDoc.createElement('div')
+  buttonContainer.attr = {
+    id: 'oauth-buttons',
+    class: 'oauth-buttons'
+  }
+  form.child[form.child.length] = form.child[form.child.length - 1]
+  form.child[form.child.length - 2] = buttonContainer
 }
--- src/server/redirect-unused.js
+++ src/server/redirect-unused.js
@@ -1,16 +1,18 @@
 const dashboard = require('@layeredapps/dashboard')
 
 module.exports = {
-  after: (req, res) => {
-    if (!req.account || !req.account.oauth) {
-      return
-    }
-    res.ended = true
-    if (req.urlPath === '/account/change-username' || req.urlPath === '/account/change-password') {
-      return dashboard.Response.redirect(req, res, '/account/convert-account?message=conversion-required-change-credential')
-    }
-    if (req.urlPath === '/account/reset-codes' || req.urlPath === '/account/create-reset-code') {
-      return dashboard.Response.redirect(req, res, '/account/convert-account?message=conversion-required-reset-code')
-    }
+  after: redirectUnused
+}
+
+async function redirectUnused (req, res) {
+  if (!req.account || !req.account.oauth) {
+    return
+  }
+  res.ended = true
+  if (req.urlPath === '/account/change-username' || req.urlPath === '/account/change-password') {
+    return dashboard.Response.redirect(req, res, '/account/convert-account?message=conversion-required-change-credential')
+  }
+  if (req.urlPath === '/account/reset-codes' || req.urlPath === '/account/create-reset-code') {
+    return dashboard.Response.redirect(req, res, '/account/convert-account?message=conversion-required-reset-code')
   }
 }
